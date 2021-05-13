5b651813021a3adf01c4d1b3cdb798ee2a9dc2aa
Nathan Premo <31483966+Grimmjow91@users.noreply.github.com>
2021-05-12 21:16:14 -0400

    I have created the main GUI, added a menu bar with menu items for new password, new users, import passwords, import users, export user, and password generator

--- PasswordManager/src/PasswordManger/Controller.java
+++ PasswordManager/src/PasswordManger/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package PasswordManger;
 
 public class Controller {
 }
--- PasswordManager/src/PasswordManger/Main.java
+++ PasswordManager/src/PasswordManger/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package PasswordManger;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
